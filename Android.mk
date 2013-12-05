f964963de80ff0fc88ee66b3eff8864d759152c1
kejar31 <justin.a.rogers@gmail.com>
2013-12-05 17:24:50 +0100

    dont build if using TG PowerHAL

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(TARGET_USES_TG_POWERHAL),true)
 ifneq ($(TARGET_USES_CM_POWERHAL),true)
 ifneq ($(TARGET_PROVIDES_POWERHAL),true)
 
@@ -17,6 +18,7 @@ LOCAL_SRC_FILES := power.c
 LOCAL_MODULE:= power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+endif
 endif
 endif
 endif
